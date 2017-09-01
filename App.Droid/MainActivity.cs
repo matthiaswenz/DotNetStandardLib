c2fd0fbfd3a84555734e8c43e3d6ea6748c3ef0c
Matthias Wenz <matwen@microsoft.com>
2017-08-31 21:14:26 -0700

    Add Android app

---
+++ App.Droid/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using StandardLibTest;
+
+namespace App.Droid
+{
+    [Activity(Label = "App", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Class1 cls = new Class1();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
